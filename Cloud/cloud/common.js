a3ec7b959b8c984740f8d5b1ba23f0569f012cd1
TomerPld <tomerpld8@gmail.com>
2015-04-25 20:41:53 +0300

    Cloud: Added basic testing functions for cloud code
     getCarMakes
     getCarModels
     getOwnedCars

---
+++ Cloud/cloud/common.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Tomer on 25/04/2015.
+ */
+function distinct (obj,fields) {
+    console.log("Implement distinct function for json");
+    if (fields === undefined) {
+
+    }
+    else {
+
+    }
+    return obj;
+}
--- Cloud/cloud/main.js
+++ Cloud/cloud/main.js
@@ -1,6 +1,67 @@
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
+});
+
+Parse.Cloud.define("getCarMakes",function(req, res) {
+    // Initialize a query object for table CarModel
+    var query = new Parse.Query("CarModel");
+    query.select(["Make"]);
+    query.find({
+            success: function (results) {
+                results = distinct(results);
+                console.log(results);
+                res.success(results);
+            },
+            error: function () {
+                res.error("Failed to retrieve car makers.");
+            }
+        });
+});
+
+Parse.Cloud.define("getCarModels", function(req, res) {
+    var make = req.params.make;
+    if (make === undefined) {
+        res.error("Car Maker is a required field.");
+    }
+    else {
+        var query = new Parse.Query("CarModel");
+        query.equalTo("Make", make);
+        query.find({
+            success: function (results) {
+                results = distinct(results);
+                console.log(results);
+                res.success(results);
+            },
+            error: function () {
+                res.error("Failed to retrieve car models for maker " + make);
+            }
+        });
+    }
+});
+
+Parse.Cloud.define("getOwnedCars", function(req, res) {
+    var user = req.user;
+    if (user === undefined) {
+        res.error("You must be logged in");
+    }
+    else {
+        console.log(user);
+        var query = Parse.Query("Car");
+        query.equalTo("Owner", {
+            __type: "Pointer",
+            className: "_User",
+            objectId: user.objectId
+        });
+        query.find({
+            success: function (results) {
+                results = distinct(results);
+                console.log(results);
+                res.success(results);
+            },
+            error: function () {
+                res.error("Failed to retrieve owned cars");
+            }
+        });
+    }
 });
