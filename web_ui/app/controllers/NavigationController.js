9524c54f624cd8359630848953d1704a0cf50074
matansab <matansabag202@gmail.com>
2015-05-11 20:53:00 +0300

    added signup signout func

--- web_ui/app/controllers/NavigationController.js
+++ web_ui/app/controllers/NavigationController.js
@@ -4,9 +4,28 @@
 app.controller('NavigationController', function ($scope, $location) {
     'use strict';
     (function() {
-        $scope.logged = false;
+        var currentUser = Parse.User.current();
+        if (currentUser) {
+            console.log("you are logged in");
+            $scope.logged = true;
+        }
+        else {
+            console.log("nobody logged");
+            $scope.logged = false;
+        }
     })();
     $scope.isActive = function (path) {
         return ($location.path() === path);
+    };
+    $scope.$on('UserLoggedIn', function() {
+        console.log("triggered");
+       $scope.logged = true;
+    });
+    $scope.doLogOut = function() {
+        Parse.User.logOut();
+        if (Parse.User.current()) {
+            // TODO - logout failed
+        }
+        $scope.logged = false;
     };
 });
