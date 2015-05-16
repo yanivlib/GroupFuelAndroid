0af3dea6036f1f41f4302920fe130e1a48c351c6
yanivlib <yanivlib@gmail.com>
2015-05-16 14:10:16 +0300

    added option to access current instance of fragments

--- Android/app/src/main/java/com/mty/groupfuel/FragmentPagerAdapter.java
+++ Android/app/src/main/java/com/mty/groupfuel/FragmentPagerAdapter.java
@@ -4,6 +4,8 @@ package com.mty.groupfuel;
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.SparseArray;
+import android.view.ViewGroup;
 
 import com.mty.groupfuel.datamodel.Car;
 
@@ -11,6 +13,7 @@ public class FragmentPagerAdapter extends android.support.v4.app.FragmentPagerAd
     final int PAGE_COUNT = 3;
     private String tabTitles[] = new String[] { "My Usage", "Fuel Now", "My Account" };
     private Context context;
+    SparseArray<Fragment> registeredFragments = new SparseArray<>(3);
 
     public FragmentPagerAdapter(FragmentManager fm, Context context) {
         super(fm);
@@ -34,7 +37,22 @@ public class FragmentPagerAdapter extends android.support.v4.app.FragmentPagerAd
 
     @Override
     public CharSequence getPageTitle(int position) {
-        // Generate title based on item position
         return tabTitles[position];
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Fragment fragment = (Fragment) super.instantiateItem(container, position);
+        registeredFragments.put(position, fragment);
+        return fragment;
+    }
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        registeredFragments.remove(position);
+        super.destroyItem(container, position, object);
+    }
+
+    public Fragment getRegisteredFragment(int position) {
+        return registeredFragments.get(position);
     }
 }
