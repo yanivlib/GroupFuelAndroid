415f59e5a33585fd96184d0807550ec1ea859deb
yanivlib <yanivlib@gmail.com>
2015-05-24 16:05:45 +0300

    added new class for constansts

---
+++ Android/app/src/main/java/com/mty/groupfuel/Consts.java
@@ -0,0 +1,9 @@
+package com.mty.groupfuel;
+
+public class Consts {
+    public final static String PARENT_ACTIVITY_NAME = "parent_activity_name";
+    public final static String BUTTON_ADDREMOVE = "button_addremove";
+    public final static String BUTTON_UPDATE = "button_update";
+    public final static String BUTTON_LOGOUT = "button_logout";
+    public final static String DATE_FORMAT = "MM/dd/yy";
+}
