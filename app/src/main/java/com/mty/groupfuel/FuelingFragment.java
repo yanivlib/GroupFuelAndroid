59db8e40f70a5c6461cc8486316eead543305a5c
yanivlib <yanivlib@gmail.com>
2015-06-25 11:27:26 +0300

    fueling now works in background

--- app/src/main/java/com/mty/groupfuel/FuelingFragment.java
+++ app/src/main/java/com/mty/groupfuel/FuelingFragment.java
@@ -217,7 +217,6 @@ public class FuelingFragment extends android.support.v4.app.Fragment {
                         MainActivity.createErrorAlert(e.getMessage(), context).show();
                     } else {
                         Toast.makeText(context, context.getString(R.string.fueling_updated), Toast.LENGTH_LONG).show();
-                        getActivity().getSupportFragmentManager().popBackStack();
                     }
                 }
             });
@@ -225,6 +224,7 @@ public class FuelingFragment extends android.support.v4.app.Fragment {
             mileageEditText.setText(null);
             priceEditText.setText(null);
             locationEditText.setText(null);
+            getActivity().getSupportFragmentManager().popBackStack();
         }
     }
 }
